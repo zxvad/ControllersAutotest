629583bd3787cf04b8ef0fa3f9f0d50ee02d12d0
wi1ser <ser_wil@mail.ru>
2014-03-02 16:34:52 +0300

    Добавлен класс MSQLDatevaseDriver

---
+++ math/BDsystem/MSQLDatebaseDriver.cpp
@@ -0,0 +1,21 @@
+/*
+ * MSQLDatebaseDriver.cpp
+ *
+ *  Created on: 02 марта 2014 г.
+ *      Author: Вилсер
+ */
+
+#include "MSQLDatebaseDriver.h"
+
+namespace BDdriver {
+
+MSQLDatebaseDriver::MSQLDatebaseDriver() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MSQLDatebaseDriver::~MSQLDatebaseDriver() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace BDdriver */
