6e4de00a6f410252611fc6abcd929a9e4bc1a603
wi1ser <ser_wil@mail.ru>
2014-03-02 16:33:40 +0300

    Добавлен класс FacadeBD

---
+++ math/BDsystem/FacadeBD.cpp
@@ -0,0 +1,21 @@
+/*
+ * FacadeBD.cpp
+ *
+ *  Created on: 02 марта 2014 г.
+ *      Author: Вилсер
+ */
+
+#include "FacadeBD.h"
+
+namespace BDdriver {
+
+FacadeBD::FacadeBD() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FacadeBD::~FacadeBD() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace BDdriver */
