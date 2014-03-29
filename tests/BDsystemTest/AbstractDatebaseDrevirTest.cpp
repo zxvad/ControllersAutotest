2ca5ff87e3a032c7ecd8ef7c754491b07cc4b24a
wi1ser <ser_wil@mail.ru>
2014-03-29 16:05:39 +0300

    Добавил тесты на классы бд

---
+++ tests/BDsystemTest/AbstractDatebaseDrevirTest.cpp
@@ -0,0 +1,21 @@
+/*
+ * AbstractDatebaseDrevirTest.cpp
+ *
+ *  Created on: 29 марта 2014 г.
+ *      Author: Вилсер
+ */
+
+#include "AbstractDatebaseDrevirTest.h"
+
+namespace BDdriverTest {
+
+AbstractDatebaseDrevirTest::AbstractDatebaseDrevirTest() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AbstractDatebaseDrevirTest::~AbstractDatebaseDrevirTest() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace BDdriverTest */
---
+++ tests/BDsystemTest/MSQLDatebaseDriverTest.h
@@ -0,0 +1,21 @@
+/*
+ * MSQLDatebaseDriverTest.h
+ *
+ *  Created on: 29 марта 2014 г.
+ *      Author: Вилсер
+ */
+
+#ifndef MSQLDATEBASEDRIVERTEST_H_
+#define MSQLDATEBASEDRIVERTEST_H_
+
+namespace BDdriverTest {
+
+class MSQLDatebaseDriverTest {
+public:
+	MSQLDatebaseDriverTest();
+	virtual ~MSQLDatebaseDriverTest();
+};
+
+} /* namespace BDdriverTest */
+
+#endif /* MSQLDATEBASEDRIVERTEST_H_ */
