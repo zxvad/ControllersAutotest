a61b04a61d4463352634fbb649a62d64aa00a24e
Timofey Lavrenenko <timon.ltv@gmail.com>
2014-04-13 14:32:15 +0400

    Added GateSensor state processing

--- math/SCADAEvents/Sensors/GateSensor.cpp
+++ math/SCADAEvents/Sensors/GateSensor.cpp
@@ -13,6 +13,7 @@ namespace Sensors
 GateSensor::GateSensor(const char* tagName, const float value):
 		SensorFloatValue(tagName, value)
 {
+	m_state = GateState::UNDEFINED;
 }
 
 GateSensor::~GateSensor()
@@ -22,22 +23,27 @@ GateSensor::~GateSensor()
 
 void GateSensor::beginOpening()
 {
+	m_state = GateState::OPENING;
 }
 
 void GateSensor::beginClosing()
 {
+	m_state = GateState::CLOSING;
 }
 
 void GateSensor::stopCurrentOperation()
 {
+	m_state = GateState::STOPPED;
 }
 
 bool GateSensor::isOpen() const
 {
+	return m_state == GateState::OPEN;
 }
 
 bool GateSensor::isClose() const
 {
+	return m_state == GateState::CLOSED;
 }
 
 void GateSensor::ecuCrashed()
